b35da16a0c6043c3de3cfb73ffc6921b3081c872
Andrew Gal <galandras12@gmail.com>
2020-10-14 14:43:40 +0200

    I not need interval.

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ client.on('ready', () => {
   console.log('BOT is RUN');
   client.channels.cache.get("ChannelID_BOTMAIN").send("Andrew BOT is `RESTARTED`");
   
-  setInterval(function(){
+ /* setInterval(function(){
 client.channels.cache.get("ChannelID1").bulkDelete(100).catch(console.error);
 console.log('DELETED last 100 messages in channelname!');
 client.channels.cache.get("ChannelID_BOTMAIN").send("`DELETED` last 100 messages in `channelname`");
@@ -25,7 +25,7 @@ client.channels.cache.get("ChannelID_BOTMAIN").send("`DELETED` last 100 messages
 client.channels.cache.get("ChannelID3").bulkDelete(100).catch(console.error);
 console.log('DELETED last 100 messages in channelname!');
 client.channels.cache.get("ChannelID_BOTMAIN").send("`DELETED` last 100 messages in `channelname`");
-  }, 1000*60*60 ); // 1000 ms * 60s * 120/60min 
+  }, 1000*60*60 ); // 1000 ms * 60s * 120/60min */
   
   /*client.user.setPresence({
         status: "online",  //You can show online, idle....
@@ -55,23 +55,38 @@ client.on("message", function(message) {
     message.reply(`az összege: ${sum}!`);
   }
   
-  else if (command === "del") {
-	 message.channel.bulkDelete(100);
-	/* var interval = setInterval (function () {
-            // use the message's channel (TextChannel) to send a new message
-            message.channel.bulkDelete(100)
-            .catch(console.error);
-			console.log('DEL! 100 message ACTIVATED again!');			// add error handling here
+else if (command === "dela") {
+	client.channels.cache.get("ChannelID1").bulkDelete(100).catch(console.error);
+	console.log('DELETED last 100 messages in Channnel2!');
+	client.channels.cache.get("ChannelID_BOTMAIN").send("`DELETED` last 100 messages in `Channel1`");
+}
+	
+else if (command === "delb") {
+	client.channels.cache.get("ChannelID2").bulkDelete(100).catch(console.error);
+	console.log('DELETED last 100 messages in Channel2!');
+	client.channels.cache.get("ChannelID_BOTMAIN").send("`DELETED` last 100 messages in `Channel2`");
+}
+
+else if (command === "delc") {
+	client.channels.cache.get("ChannelID3").bulkDelete(100).catch(console.error);
+	console.log('DELETED last 100 messages in Channel3!');
+	client.channels.cache.get("Channel_BOTMAIN").send("`DELETED` last 100 messages in `Channel3!`");
+}
+
+  /* else if (command === "del") {
+	message.channel.bulkDelete(100);
+	console.log('DEL! 100 message ACTIVATED again!');
         }, 1000*60*60 );
-//OFF
-		*/
+//OFF	
 	 console.log('DEL command is active!');
- } 
+ } */
 
 else if (command === "help") {
 message.reply("`használd a következő paancsokat: `\n`Válasz időt mutatja a bot és a géped között: !ping`\n`Számok gyors összeadására van: !szum`");
 }
-
+else if (command === "adhelp") {
+		message.reply("`használd a következő admin paancsokat: `\n`Közlemény törlés: !delkoz`\n`Pénzügy eAdat törlése: !delpadat`\n`Adó eAdat törlése: !delaadat`");
+}
 
 });
 
